93f9e7537f1b117654f18b2162a444b10a548f1b
fukur <fukur@LAPTOP-7EQMGM6H>
2020-05-07 21:15:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp9/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A = new System.Windows.Forms.TextBox();
+            this.H = new System.Windows.Forms.TextBox();
+            this.B = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rez = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A
+            // 
+            this.A.Location = new System.Drawing.Point(28, 49);
+            this.A.Name = "A";
+            this.A.Size = new System.Drawing.Size(211, 22);
+            this.A.TabIndex = 0;
+            // 
+            // H
+            // 
+            this.H.Location = new System.Drawing.Point(28, 205);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(211, 22);
+            this.H.TabIndex = 1;
+            // 
+            // B
+            // 
+            this.B.Location = new System.Drawing.Point(28, 123);
+            this.B.Name = "B";
+            this.B.Size = new System.Drawing.Size(211, 22);
+            this.B.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите а:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Введите b:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Введите h:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::WindowsFormsApp9.Properties.Resources._123;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 49);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(206, 96);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(206, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rez
+            // 
+            this.rez.Location = new System.Drawing.Point(503, 36);
+            this.rez.Multiline = true;
+            this.rez.Name = "rez";
+            this.rez.ReadOnly = true;
+            this.rez.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.rez.Size = new System.Drawing.Size(201, 185);
+            this.rez.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(500, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(80, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Результат:";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.rez);
+            this.panel1.Location = new System.Drawing.Point(12, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(725, 246);
+            this.panel1.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(262, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(206, 36);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Очистить поле результата";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 279);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.B);
+            this.Controls.Add(this.H);
+            this.Controls.Add(this.A);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox A;
+        private System.Windows.Forms.TextBox H;
+        private System.Windows.Forms.TextBox B;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox rez;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp9/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static void f(double x, out double y)
+        {      
+            if (x > 0) y = x;
+            else if ((-1) <= x && x <= 0) y = 0;
+            else y = x * x;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(A.Text);
+                double b = double.Parse(B.Text);
+                double h = double.Parse(H.Text);
+                double y;
+                if (h > 0 && b > a)
+                {
+
+                    for (double i = a; i <= b; i += h)
+                    {
+                        f(i, out y);
+                        rez.Text += "f(";
+                        rez.Text += i;
+                        rez.Text += ") = ";
+                        rez.Text += y;
+                        rez.Text += Environment.NewLine;
+                    }
+                }
+                else { rez.Text = "Некорректный ввод!"; }
+            }
+            catch { rez.Text = "Некорректный ввод!"; }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            rez.Clear();
+        }
+    }
+}
